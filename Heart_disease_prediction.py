fee6f2bab34727d685912ec8f759369b41a1bfb1
Sonakshi <118465247+Sonakshi111@users.noreply.github.com>
2025-05-31 13:18:46 +0530

    Update Heart_disease_prediction.py

--- Heart_disease_prediction.py
+++ Heart_disease_prediction.py
@@ -29,45 +29,21 @@ if (selected == 'Heart Disease Prediction'):
     st.title('Heart Disease Prediction using Ml')
     
     #Getting input from user
-    #Creating columns
-    col1,col2,col3 = st.columns(3)
-    
-    with col1:
-        age= st.text_input('Age')
-    
-    with col2:
-        sex = st.text_input('Sex')
-        
-    with col3:    
-        cp = st.text_input('Chest Pain types')
-    
-    with col1:
-        trestbps = st.text_input('Resting Blood Pressure')
-        
-    with col2:
-        chol = st.text_input('Serum Cholestrol in mg/dl')
-        
-    with col3:
-        fbs = st.text_input('Fasting Blood Sugar >129 mg/dl')
-        
-    with col1:
-        restecg = st.text_input('Resting Electronicdiographic Results')
-        
-    with col2: 
-        thalach = st.text_input('Maximum Heart Rate achieved')
-        
-    with col3:
-        exang = st.text_input('Exercise Induced Angina')
-        
-    with col1:
-        oldpeak = st.text_input('ST depression induced by exercise')
-    with col2: 
-        slope = st.text_input('Slope of the peak exercise ST segment')
-    with col3:
-        ca = st.text_input('Major vessels colored by flourosopy')
-        
-    with col1:
-        thal = st.text_input('thal:0=normal; 1=fixed defect; 2=reversable defect')
+    # page title
+    st.title('Heart Disease Prediction using Ml')
+    age= st.text_input('Age')
+    sex = st.text_input('Sex  ( 0 = Female || 1 = Male )')
+    cp = st.text_input('Chest Pain types  ( 0 = Typical angina || 1 = Atypical || 2 = Non-anginal || 3 = Asymptomatic)')
+    trestbps = st.text_input('Resting Blood Pressure ( Range = 94 – 200 mm Hg )')
+    chol = st.text_input('Serum Cholestrol in mg/dl ( Range = 126 – 564 mg/dl )')
+    fbs = st.text_input('Fasting Blood Sugar >120 mg/dl ( 0 = No || 1 = Yes )')
+    restecg = st.text_input('Resting Electronicdiographic Results ( 0 = Normal || 1 = ST-T abnormality || 2 = Left ventricular hypertrophy)')
+    thalach = st.text_input('Maximum Heart Rate achieved ( 71 – 202 bpm )')
+    exang = st.text_input('Exercise Induced Angina ( 0 = No || 1 = Yes )')
+    oldpeak = st.text_input('ST depression induced by exercise ( Range = 0.0 – 6.2 )')
+    slope = st.text_input('Slope of the peak exercise ST segment ( 0 = Upsloping || 1 = Flat || 2 = Downsloping )')
+    ca = st.text_input('Major vessels colored by flourosopy ( Range = 0 – 3 )')
+    thal = st.text_input('Blood disorder type  ( 1 = Normal || 2 = Fixed defect || 3 = Reversible defect )')
         
     
     #code for prediction 
